b6ceb4317c0037ad0ca2aa1143319b29dabf410d
Valet-V0ult-de-Fur1e <82310399+Valet-V0ult-de-Fur1e@users.noreply.github.com>
2024-02-20 20:45:03 +0300

    delete some folders

---
+++ prj.codeforces/0071a.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+int main() {
+	int count_words;
+	std::cin >> count_words;
+	for (int i = 0; i < count_words; ++i) {
+		std::string word;
+		std::cin >> word;
+		if (word.length > 10) {
+			std::cout << word.front() << word.length - 2 << word.back() << std::endl;
+		}
+		else {
+			std::cout << word << std::endl;
+		}
+	}
+}
